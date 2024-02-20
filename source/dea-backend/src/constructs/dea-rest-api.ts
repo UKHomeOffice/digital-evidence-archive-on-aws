34cdbf95dda4e2aacba1f751d61cf5eb2d0e0d33
Samiwel Thomas <samiwel.thomas@digital.homeoffice.gov.uk>
2024-02-20 13:18:49 +0000

    Remove traceEnabled at deploy level

--- source/dea-backend/src/constructs/dea-rest-api.ts
+++ source/dea-backend/src/constructs/dea-rest-api.ts
@@ -178,7 +178,6 @@ export class DeaRestApiConstruct extends Construct {
       },
       deployOptions: {
         stageName: STAGE,
-        tracingEnabled: true,
         metricsEnabled: true,
         // Per method throttling limit. Conservative setting based on fact that we have 35 APIs and Lambda concurrency is 1000
         // Worst case this setting could potentially initiate up to 1750 API calls running at any moment (which is over lambda limit),
@@ -191,6 +190,7 @@ export class DeaRestApiConstruct extends Construct {
             throttlingRateLimit: 40,
             metricsEnabled: true,
             loggingLevel: MethodLoggingLevel.INFO,
+            dataTraceEnabled: true,
           },
           // /availableEndpoints reads data from the Parameter Store.
           // Default throughput: 40 (Shared by the following API actions: GetParameter, GetParameters, GetParametersByPath)
