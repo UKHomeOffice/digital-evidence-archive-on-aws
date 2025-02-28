bcbcc46e584226eb3e0b54be7cf1b23dd2e0dae4
rudolf <rudolf.castelino@homeoffice.gov.uk>
2025-02-28 11:37:27 +0000

    Checking the contents of the DEACASE file that is being saved

--- source/dea-app/src/persistence/case-file.ts
+++ source/dea-app/src/persistence/case-file.ts
@@ -50,6 +50,7 @@ export const completeCaseFileUpload = async (
     { transaction }
   );
 
+  console.log('completeCaseFileUpload: ', deaCaseFile);
   await repositoryProvider.CaseModel.update(
     {
       PK: `CASE#${deaCaseFile.caseUlid}#`,
