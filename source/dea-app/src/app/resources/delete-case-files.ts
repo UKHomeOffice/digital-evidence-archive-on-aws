c988d47d645b3f3d1ab1c98494c7a3876da93192
rudolf <rudolf.castelino@homeoffice.gov.uk>
2025-03-21 14:57:02 +0000

    deleting  s3objects

--- source/dea-app/src/app/resources/delete-case-files.ts
+++ source/dea-app/src/app/resources/delete-case-files.ts
@@ -8,6 +8,8 @@ import { DeleteCaseFilesDTO } from '../../models/case-file';
 import { caseFileDeleteRequestSchema } from '../../models/validation/case-file';
 import { joiUlid } from '../../models/validation/joi-common';
 import { defaultProvider } from '../../persistence/schema/entities';
+import { DatasetsProvider, defaultDatasetsProvider } from '../../storage/datasets';
+import { NotFoundError } from '../exceptions/not-found-exception';
 import * as CaseService from '../services/case-service';
 import { DEAGatewayProxyHandler } from './dea-gateway-proxy-handler';
 import { responseNoContent } from './dea-lambda-utils';
@@ -17,11 +19,16 @@ export const deleteCaseFiles: DEAGatewayProxyHandler = async (
   context,
   /* the default case is handled in e2e tests */
   /* istanbul ignore next */
-  repositoryProvider = defaultProvider
+  repositoryProvider = defaultProvider,
+  datasetsProvider: DatasetsProvider = defaultDatasetsProvider
 ) => {
   const caseId = getRequiredPathParam(event, 'caseId', joiUlid);
 
-  // const deaCase = await CaseService.getCase(caseId, repositoryProvider);
+  const deaCase = await CaseService.getCase(caseId, repositoryProvider);
+  if (!deaCase) {
+    throw new NotFoundError(`Could not find case: ${caseId}`);
+  }
+
   const deleteCaseFilesDTO: DeleteCaseFilesDTO = getRequiredPayload(
     event,
     'Delete cases',
@@ -29,7 +36,12 @@ export const deleteCaseFiles: DEAGatewayProxyHandler = async (
   );
 
   if (caseId && deleteCaseFilesDTO) {
-    await CaseService.deleteCaseFiles(caseId, deleteCaseFilesDTO.filesToDelete, repositoryProvider);
+    await CaseService.deleteCaseFiles(
+      deaCase,
+      deleteCaseFilesDTO.filesToDelete,
+      repositoryProvider,
+      datasetsProvider
+    );
   } else {
     console.log('No files to delete');
   }
